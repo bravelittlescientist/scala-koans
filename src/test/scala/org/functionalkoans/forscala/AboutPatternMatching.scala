d295bf6b2223515b78558c7de8577f3b23f70239
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-08-21 15:19:24 -0700

    Finished pattern-matching koans.

--- src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPatternMatching.scala
@@ -16,7 +16,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0 //case _ will trigger if all other cases fail.
     }
 
-    myStuff should be(__)
+    myStuff should be(2)
 
   }
 
@@ -30,7 +30,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => println(stuff); 0
     }
 
-    myStuff should be(__, __, __)
+    myStuff should be(0, 0, 255)
 
   }
 
@@ -44,7 +44,7 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Mama")) should be(__)
+    goldilocks(("porridge", "Mama")) should be("Mama eating porridge")
 
   }
 
@@ -57,8 +57,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
 
   }
 
@@ -71,8 +71,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("Papa said someone's been eating my porridge")
+    goldilocks(("chair", "Mama")) should be("Mama said someone's been sitting in my chair")
   }
 
 
@@ -88,8 +88,8 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks("Eating Alert: bear=Papa, source=porridge") should be(__)
-    goldilocks("Sitting Alert: bear=Mama, source=chair") should be(__)
+    goldilocks("Eating Alert: bear=Papa, source=porridge") should be("Papa said someone's been eating my porridge")
+    goldilocks("Sitting Alert: bear=Mama, source=chair") should be("Mama said someone's been sitting on my chair")
   }
 
   koan( """A backquote can be used to refer to a stable variable in scope to create a case statement.
@@ -103,10 +103,10 @@ class AboutPatternMatching extends KoanSuite {
       case _ => "what?"
     }
 
-    goldilocks(("porridge", "Papa")) should be(__)
-    goldilocks(("chair", "Mama")) should be(__)
-    goldilocks(("porridge", "Cousin")) should be(__)
-    goldilocks(("beer", "Cousin")) should be(__)
+    goldilocks(("porridge", "Papa")) should be("eating")
+    goldilocks(("chair", "Mama")) should be("sitting")
+    goldilocks(("porridge", "Cousin")) should be("eating")
+    goldilocks(("beer", "Cousin")) should be("what?")
   }
 
   koan("A backquote can be used to refer to a method parameter as a stable variable to create a case statement.") {
@@ -115,8 +115,8 @@ class AboutPatternMatching extends KoanSuite {
       case `i` => true
       case _ => false
     }
-    patternEquals(3, 3) should be(__)
-    patternEquals(7, 9) should be(__)
-    patternEquals(9, 9) should be(__)
+    patternEquals(3, 3) should be(true)
+    patternEquals(7, 9) should be(false)
+    patternEquals(9, 9) should be(true)
   }
 }
